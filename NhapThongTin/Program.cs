f0d0b9b5849f0ff71e95d1eaaf4a34f419d252d2
Quang1811 <you@example.com>
2021-07-07 23:54:16 +0700

    Add project files.

---
+++ NhapThongTin/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace NhapThongTin
+{
+    class Program
+{
+    static void Main(string[] args)
+    {
+        string HoTen;
+        DateTime NgaySinh;
+        int Tuoi;
+        Console.Write(" nhap Ho Ten: ");
+        HoTen = Console.ReadLine();
+        Console.WriteLine(HoTen);
+        Console.Write(" nhap Ngay Sinh: ");
+        NgaySinh = DateTime.Parse(Console.ReadLine());
+        Tuoi = DateTime.Today.Year - NgaySinh.Year;
+        string chuoi = "Tuoi:" + Tuoi;
+        Console.Write(chuoi);
+        Console.ReadLine();
+    }
+}
+}
